af5cae75ddeef4d727b48b51b7b9d6419adbbbea
Dave Woodward <dave@salte.io>
2020-08-10 17:33:56 -0500

    Handle empty CI file.

--- application/main/performers/01-runner-authorization.js
+++ application/main/performers/01-runner-authorization.js
@@ -5,11 +5,13 @@ const git = simpleGit();
 
 module.exports.perform = (refname, oldrev, newrev) => {
   return git.catFile(['blob', `${newrev}:.gitlab-ci.yml`]).then((contents) => {
-    const gitlabCiFile = yaml.parse(contents);
-    for (const [key] of Object.entries(gitlabCiFile)) {
-      if (gitlabCiFile[key]['tags'] && gitlabCiFile[key]['tags'].includes('production')) {
-        if (!gitlabCiFile[key]['only'] || gitlabCiFile[key]['only'].length > 1 || gitlabCiFile[key]['only'][0] !== 'production') {
-          throw new Error('GL-HOOK-ERR: Only the production branch can target the production Gitlab runner.');
+    if (contents) {
+      const gitlabCiFile = yaml.parse(contents);
+      for (const [key] of Object.entries(gitlabCiFile)) {
+        if (gitlabCiFile[key]['tags'] && gitlabCiFile[key]['tags'].includes('production')) {
+          if (!gitlabCiFile[key]['only'] || gitlabCiFile[key]['only'].length > 1 || gitlabCiFile[key]['only'][0] !== 'production') {
+            throw new Error('GL-HOOK-ERR: Only the production branch can target the production Gitlab runner.');
+          }
         }
       }
     }
--- application/test/performers/runner-authorization.spec.js
+++ application/test/performers/runner-authorization.spec.js
@@ -18,6 +18,8 @@ describe('The Runner Authorization Module', function() {
                 resolve(fs.readFileSync(path.resolve(__dirname, '../../files/valid.gitlab-ci.yml'), {encoding: 'utf-8'}));
               } else if (argArray[1].includes('5bb9e579cb1518ceb6a45126527aea0877854d8a')) {
                 resolve(fs.readFileSync(path.resolve(__dirname, '../../files/invalid.gitlab-ci.yml'), {encoding: 'utf-8'}));
+              } else if (argArray[1].includes('0000000000000000000000000000000000000000')) {
+                resolve(fs.readFileSync(path.resolve(__dirname, '../../files/empty.gitlab-ci.yml'), {encoding: 'utf-8'}));
               } else {
                 throw new Error('Unexpected Test Case');
               }
@@ -37,6 +39,12 @@ describe('The Runner Authorization Module', function() {
 
   it('should return success when the production runner is restricted to the production branch.', function() {
     return performer.perform('refs/heads/master', '1a6433a6fd254131a11e66f6a77fefc2c11d2a98', '943ac77e2f118d9f9950b4265f3b3c4e3639be8f').then((response) => {
+      chai.expect(response).to.be.true;
+    });
+  });
+
+  it('should return success when the CI file is empty.', function() {
+    return performer.perform('refs/heads/master', '0000000000000000000000000000000000000000', '0000000000000000000000000000000000000000').then((response) => {
       chai.expect(response).to.be.true;
     });
   });
